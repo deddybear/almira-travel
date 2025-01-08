0215af653ca66c3e9fc852905063324fa2851d85
- <->
2025-01-08 21:50:01 +0700

    proses halaman awal

--- resources/views/guest-v2/index.blade.php
+++ resources/views/guest-v2/index.blade.php
@@ -27,22 +27,127 @@
     </div>
     <div class="service-img container py-3 mt-5">
         <div class="row gap-5">
-            <div class="col-md-4" style="height:500px;background-color:black">
-
+            <div class="col-md-6 rounded-4 service-1">
+                <span class="badge text-bg-dark">Accommondation</span>
             </div>
             <div class="col-md-5">
                 <div class="row mb-2">
-                    <div class="col-md-2">
-                        <p>A</p>
+                    <div class="col-md-6 rounded-4 service-2">
+                        <span class="badge text-bg-dark">Tour Travel</span>
                     </div>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-2">
-                        <p>B</p>
+                    <div class="col-md-6 rounded-4 service-3">
+                        <span class="badge text-bg-dark">Car Rental</span>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="car-section mt-5 container p-0">
+        <div class="title-car mb-5">
+            <div class="title-car-section">
+                <h1>Ready exclusive vehicle</h1>
+            </div>
+            <p>Deals for you !</p>
+            <div class="float-end border rounded-pill button-see-all px-1">
+                <span class="m-2 fw-bold">
+                    <a class="text-black text-decoration-none" href="#">
+                        See All
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </a>
+                </span>
+            </div>
+        </div>
 
+        <div class="list-car mt-5 p-5 rounded-3">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-1.png') }}" class="card-img-top h-25" alt="card-1">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-2.png') }}" class="card-img-top h-25" alt="card-2">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-3.png') }}" class="card-img-top h-25" alt="card-3">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-4.png') }}" class="card-img-top h-25" alt="card-4">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="car-section mt-5 container p-0">
+        <div class="title-car mb-5">
+            <div class="title-car-section">
+                <h1>Ready exclusive vehicle</h1>
+            </div>
+            <p>Deals for you !</p>
+            <div class="float-end border rounded-pill button-see-all px-1">
+                <span class="m-2 fw-bold">
+                    <a class="text-black text-decoration-none" href="#">
+                        See All
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </a>
+                </span>
+            </div>
+        </div>
+
+        <div class="list-car mt-5 p-5 rounded-3">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-1.png') }}" class="card-img-top h-25" alt="card-1">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-2.png') }}" class="card-img-top h-25" alt="card-2">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-3.png') }}" class="card-img-top h-25" alt="card-3">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/images/vehicle-4.png') }}" class="card-img-top h-25" alt="card-4">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                      </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
